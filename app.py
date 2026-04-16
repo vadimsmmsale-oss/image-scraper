bc84b1f6482d30edf3a6239252d0d2e35c4f4f7f
vadimsmmsale-oss <vadim.smmsale@gmail.com>
2026-04-16 18:16:29 +0300

    app.py

--- app.py
+++ app.py
@@ -92,4 +92,5 @@ def health():
     return "OK"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
